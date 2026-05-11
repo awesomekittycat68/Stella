aa46fee330a58560d9e9f8d698e4800d326c1435
bxt <vaildbeanie@gmail.com>
2026-05-11 05:04:11 -0500

    Initial upload

---
+++ README.md
@@ -0,0 +1,5 @@
+> [!IMPORTANT]
+> this is what happens when you demote the developer that coded 90% of the server 🤦  
+> this is from november 2025, so very old src but still src!!!  
+> fuck you supergamer ✌️  
+> discord.gg/vanillarec
---
+++ Routes/NameServer.cs
@@ -0,0 +1,34 @@
+﻿using Stella.Models;
+
+namespace Stella.Routes
+{
+    public class NS
+    {
+        [ServerAPI.GET("/")]
+        public NameServerResponse NameServer()
+        {
+            return new NameServerResponse
+            {
+                Accounts = "http://ns.stellaonline.org",
+                API = "http://ns.stellaonline.org", // dont add /api because every call to this service to this already does
+                Auth = "http://ns.stellaonline.org/auth",
+                CDN = "http://ns.stellaonline.org/cdn",
+                Chat = "http://ns.stellaonline.org/chat",
+                Clubs = "http://ns.stellaonline.org/clubs",
+                Commerce = "http://ns.stellaonline.org/commerce",
+                DataCollection = "http://ns.stellaonline.org/datacollection",
+                Images = "http://ns.stellaonline.org/img",
+                Leaderboard = "http://ns.stellaonline.org",
+                Link = "http://ns.stellaonline.org/link",
+                Matchmaking = "http://ns.stellaonline.org/match",
+                Moderation = "http://ns.stellaonline.org/moderation",
+                Notifications = "http://ns.stellaonline.org/notify",
+                PlatformNotifications = "http://ns.stellaonline.org/platformnotifications",
+                RoomComments = "http://ns.stellaonline.org/roomcomments",
+                Rooms = "http://ns.stellaonline.org/rooms",
+                Storage = "http://ns.stellaonline.org/storage",
+                WWW = "http://stellaonline.org"
+            };
+        }
+    }
+}
